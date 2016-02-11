41e98a0d26dfa2e60aaca83ef2bb021724a837d1
codyss <cody.schwarz@gmail.com>
2016-02-11 17:01:08 -0500

    redirect on playlist post

--- browser/js/playlist/playlist.controller.js
+++ browser/js/playlist/playlist.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-juke.controller('PlaylistCtrl', function ($scope, PlaylistFactory) {
+juke.controller('PlaylistCtrl', function ($scope, PlaylistFactory, $state) {
   PlaylistFactory.getPlaylists()
   .then(function(res){
       $scope.playlists = res;
@@ -10,8 +10,12 @@ juke.controller('PlaylistCtrl', function ($scope, PlaylistFactory) {
   $scope.playlist = {};
 
   $scope.logPlaylist = function (data) {
-    PlaylistFactory.newPlaylist(data);
-    $scope.playlist= {};
+    PlaylistFactory.newPlaylist(data)
+    .then(playlist=> {
+      // console.log(playlist);
+      $scope.playlist= {};
+      $state.go('playlist', {Id: playlist._id});  
+    });
   }
 
   console.log('playlist hmmm')
